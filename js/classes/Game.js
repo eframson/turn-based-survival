50b19a2536623a524bd93bac459c591b704c37fb
Emmett Framson <eframson@comscore.com>
2016-09-30 15:34:05 -0700

    Added starvation mechanics, yay!

--- js/classes/Game.js
+++ js/classes/Game.js
@@ -1,3 +1,5 @@
+var $Utils = Utils.prototype;
+
 var Game = function(data) {
 	this.init(data);
 };
@@ -42,12 +44,21 @@ Game.prototype.init = function(data){
 	this.isPlaying = 0;
 	this.timerInterval = 1000;
 	this.isPopupVisible = 0;
+	this.hourlyEvents = {};
+	this.dailyEvents = {};
+	this.seasonalEvents = {};
+	this.yearlyEvents = {};
+	this.dailyMealsDesired = data.dailyMealsDesired || 0;
+	this.dailyMealsServed = data.dailyMealsServed || 0;
+	this.lowFoodDaysCount = data.lowFoodDaysCount || 0;
+	this.starvationMarkers = data.starvationMarkers || 0;
+	this.isGameOver = data.isGameOver || 0;
 
 	//Observables
-	this.availableSettlers = ko.observable(data.availableSettlers || 0);
+	this.availableSettlers = ko.observable( $Utils.setDefaultValue(data.availableSettlers, 5) );
 	this.workingSettlers = ko.observable(data.workingSettlers || 0);
 	this.busySettlers = ko.observable(data.busySettlers || 0);
-	this.resources = ko.observable(data.resources || {});
+	this.resources = ko.observable(data.resources || this._getDefaultResources());
 	this.playSpeed = ko.observable(1);
 
 	//Computed
@@ -62,45 +73,10 @@ Game.prototype.init = function(data){
 	document.getElementById("current-season").innerHTML = this.currentSeason;
 	document.getElementById("current-year").innerHTML = this.yearCounter;
 
+	this._setupDefaultTimeEvents();
+
 	this.somePerson = new Person({ name : 'Bob' });
 	this.someSettler = new Settler({ name : 'Jeff', tribe : 'Umpqua' });
-}
-
-//Update existing props on an existing observable and add new ones
-Game.prototype._updateObservableProp = function(obs, newData, clearUnusedKeys, addNewKeys) {
-
-	if( !ko.isObservable(obs) ){
-		console.log("_updateObservableProp requires an observable as its first argument");
-		return false;
-	}
-
-	clearUnusedKeys 	= Utils.setDefaultValue(clearUnusedKeys, 0);
-	addNewKeys 			= Utils.setDefaultValue(addNewKeys, 1);
-	var current 		= obs();
-	var missingProps	= [];
-
-	for (existingProp in current){
-		if( newData[existingProp] != undefined ){
-			current[existingProp] = newData[existingProp];
-			delete newData[existingProp];
-		}else{
-			missingProps.push(existingProp);
-		}
-	}
-
-	if(clearUnusedKeys){
-		$.each( missingProps, function(idx, missingProp){
-			delete current[missingProp];
-		});
-	}
-
-	if(addNewKeys){
-		for (newProp in newData){
-			current[newProp] = newData[newProp];
-		}
-	}
-
-	obs(current);
 }
 
 Game.prototype.startMainTimer = function() {
@@ -126,31 +102,121 @@ Game.prototype.mainLoop = function() {
 	this.hourCounter++;
 	this.currentHour = this.hourCounter % 24;
 	document.getElementById("current-hour").innerHTML = this.currentHour;
+	this.processHourlyEvents();
 
 	if(this.currentHour == 0){
 		this.dayCounter++;
 		this.currentDay = this.dayCounter % 30;
 		document.getElementById("current-day").innerHTML = this.currentDay;
+		this.processDailyEvents();
 
 		if(this.currentDay == 0){
 			this.seasonCounter++;
 			this.currentSeasonIdx = this.seasonCounter % 4;
 			this.currentSeason = this.seasons[this.currentSeasonIdx];
 			document.getElementById("current-season").innerHTML = this.currentSeason;
+			this.processSeasonalEvents();
 
 			if(this.currentSeasonIdx == 0){
 				this.yearCounter++;
 				document.getElementById("current-year").innerHTML = this.yearCounter;
+				this.processYearlyEvents();
 			}
 		}
-	}	
+	}
 }
+
+Game.prototype.processHourlyEvents = function(){
+	var eventsForThisHour = this.hourlyEvents[this.currentHour];
+
+	if(eventsForThisHour != undefined){
+		$.each( eventsForThisHour, function(idx, hourlyEvent){
+			hourlyEvent();
+		});
+	}
+}
+
+Game.prototype.processDailyEvents = function(){
+	var eventsForThisDay = this.dailyEvents[this.currentDay];
+
+	if(eventsForThisDay != undefined){
+		$.each( eventsForThisDay, function(idx, dailyEvent){
+			dailyEvent();
+		});
+	}
+
+	if( (this.dailyMealsServed / this.dailyMealsDesired) < 0.6){
+		this.starvationMarkers++;
+		if(this.starvationMarkers > 10){
+			this.starvationMarkers = 10;
+		}
+		console.log("Starvation markers: " + this.starvationMarkers);
+	}else{
+		this.starvationMarkers-=2;
+		if(this.starvationMarkers < 0){
+			this.starvationMarkers = 0;
+		}
+		console.log("Starvation markers: " + this.starvationMarkers);
+	}
+
+	if(this.starvationMarkers > 3){
+		this.processStarvationEvent();
+	}
+
+	//Reset meal counters
+	this.dailyMealsDesired = 0;
+	this.dailyMealsServed = 0;
+}
+
+Game.prototype.processSeasonalEvents = function(){
+
+}
+
+Game.prototype.processYearlyEvents = function(){
+
+}
+
+Game.prototype.consumeFoodEvent = function(){
+	var currentFood = this.resources().food;
+	var consumedFood = this.totalSettlers();
+	this.dailyMealsDesired+=consumedFood;
+	var leftoverFood = currentFood - consumedFood;
+	var mealsServed = (leftoverFood < 0) ? consumedFood + leftoverFood : consumedFood ;
+	this.dailyMealsServed+=mealsServed;
+	this._updateObservableProp(this.resources, {food : currentFood - mealsServed});
+	console.log("Food leftover after serving: " + this.resources().food);
+}
+
+Game.prototype.processStarvationEvent = function(){
+	var roll = $Utils.doRand(1, 11);
+	if(roll <= this.starvationMarkers){
+
+		if(this.availableSettlers() > 0){
+			this.availableSettlers( this.availableSettlers() - 1 );
+			console.log("A settler has died. Num left: " + this.totalSettlers());
+		}else if(this.busySettlers() > 0){
+			this.busySettlers( this.busySettlers() - 1 );
+			console.log("A settler has died. Num left: " + this.totalSettlers());
+		}
+		if(this.totalSettlers() == 0){
+			this.gameOver();
+		}
+	}
+}
+
+Game.prototype.gameOver = function(){
+	console.log('No settlers left.');
+	this.pauseMainTimer();
+	this.isGameOver = 1;
+}
+
+//Time controls
 
 Game.prototype.spcAction = function(){
 	if(!this.isPopupVisible){
 		if(this.isPlaying){
 			this.pauseMainTimer();
-		}else{
+		}else if (!this.isGameOver){
 			this.startMainTimer();
 		}
 		this.isPlaying = !this.isPlaying;
@@ -191,6 +257,62 @@ Game.prototype._setIntervalBasedOnPlaySpeedInteger = function(){
 	}else if(this.playSpeed() == 4){
 		this.timerInterval = 100;
 	}
+}
+
+/**
+ * Update existing properties on an existing observable and optionally add new ones
+ * @param {observable} obs - A reference to an observable
+ * @param {object} newData - An object containing key+value pairs of the properties to set on the observable
+ * @param {boolean} clearUnusedKeys - Whether or not to delete keys on the existing observable that do not exist in the newData object (default: 0)
+ * @param {boolean} addNewKeys - Whether or not to add new keys to the existing observable if they are defined in the newData object (default: 1)
+ */
+Game.prototype._updateObservableProp = function(obs, newData, clearUnusedKeys, addNewKeys) {
+
+	if( !ko.isObservable(obs) ){
+		console.log("_updateObservableProp requires an observable as its first argument");
+		return false;
+	}
+
+	clearUnusedKeys 	= $Utils.setDefaultValue(clearUnusedKeys, 0);
+	addNewKeys 			= $Utils.setDefaultValue(addNewKeys, 1);
+	var current 		= obs();
+	var missingProps	= [];
+
+	for (existingProp in current){
+		if( newData[existingProp] != undefined ){
+			current[existingProp] = newData[existingProp];
+			delete newData[existingProp];
+		}else{
+			missingProps.push(existingProp);
+		}
+	}
+
+	if(clearUnusedKeys){
+		$.each( missingProps, function(idx, missingProp){
+			delete current[missingProp];
+		});
+	}
+
+	if(addNewKeys){
+		for (newProp in newData){
+			current[newProp] = newData[newProp];
+		}
+	}
+
+	obs(current);
+}
+
+Game.prototype._setupDefaultTimeEvents = function(){
+	var self = this;
+	$.each( [7, 12, 18], function(idx, hour){
+		self.hourlyEvents[hour] = $Utils.pushOntoArray(self.hourlyEvents[hour], function() {self.consumeFoodEvent();} )
+	});
+}
+
+Game.prototype._getDefaultResources = function(){
+	return {
+		food : 105
+	};
 }
 
 //This is just temporary stuff to stop JS errors
--- js/classes/Utils.js
+++ js/classes/Utils.js
@@ -9,3 +9,84 @@ var Utils = function() {
 Utils.prototype.setDefaultValue = function(param, defaultValue){
 	return (param != undefined) ? param : defaultValue ;
 }
+
+Utils.prototype.pushOntoArray = function(targetArray, valToPush){
+	if ( typeof targetArray == 'undefined' || !(targetArray instanceof Array) ) {
+		targetArray = [];
+	}
+	targetArray.push(valToPush);
+	return targetArray;
+}
+
+//Generate a number between min (inclusive) and max (exclusive)
+Utils.prototype.doRand = function(min, max){
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+Utils.prototype.doBasedOnPercent = function(percentageActions, finallyAction){
+	var percents;
+	if(percentageActions == undefined){
+		return false;
+	}
+
+	var discretePercents = Object.keys(percentageActions);
+	var percents = Array();
+
+	for(i = 0; i < discretePercents.length; i++){
+
+		if( percentageActions[discretePercents[i]].constructor == Array ){
+
+			for(j=0; j < percentageActions[discretePercents[i]].length; j++){
+				percents.push(discretePercents[i]);
+			}
+
+		}else{
+			percents.push(discretePercents[i]);
+		}
+
+	}
+
+	percents.sort();
+	
+	var rand = this.doRand(1,101);
+	var percentOffset = 0;
+	
+	for(i = 0; i < percents.length; i++){
+
+		var targetPercentage = parseInt(percents[i]);
+		var addToPercentOffset = parseInt(targetPercentage);
+
+		if( (rand - percentOffset) <= percents[i] ){
+
+			var chosenAction = percentageActions[percents[i]];
+
+			if( chosenAction.constructor == Array ){
+
+				var numPossibilities = chosenAction.length;
+				var whichPossibilityIndex = this.doRand(0,numPossibilities);
+				chosenAction = chosenAction[whichPossibilityIndex];
+
+			}
+
+			if(typeof chosenAction === 'function'){
+				return chosenAction(rand);
+			}else{
+				return chosenAction;
+			}
+
+		}else{
+			percentOffset += addToPercentOffset;
+		}
+	}
+	
+	if(finallyAction && typeof finallyAction === 'function'){
+		return finallyAction(rand);
+	}
+	
+	return false;
+}
+
+Utils.prototype.chooseRandomly = function(opts){
+	var randIdx = this.doRand(0, opts.length);
+	return opts[randIdx];	
+}
