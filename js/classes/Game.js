09a0578bf8680568c1fdb87db7c75b2f84e64a7b
Emmett Framson <eframson@comscore.com>
2016-10-03 16:09:13 -0700

    Mocked up an intro sequence with incrementally revealed text

--- js/classes/Game.js
+++ js/classes/Game.js
@@ -235,16 +235,16 @@ Game.prototype.killOldAdults = function(numOldAdults){
 	for(var i = 0; i < numOldAdults; i++){
 		if( this.availableSettlers() > 0 ){
 			this.availableSettlers( this.availableSettlers() - 1 );
-			console.log("A settler has died of old age");
+			console.log("A settler has died of old age. Num left: " + this.totalSettlers());
 		}else if ( this.workingSettlers() > 0 ){
 			this.workingSettlers( this.workingSettlers() - 1 );
-			console.log("A settler has died of old age");
+			console.log("A settler has died of old age. Num left: " + this.totalSettlers());
 		}else if(this.busySettlers() > 0){
 			this.busySettlers( this.busySettlers() - 1 );
-			console.log("A settler has died. Num left: " + this.totalSettlers());
+			console.log("A settler has died of old age. Num left: " + this.totalSettlers());
 		}
 
-		if( this.availableSettlers() == 0 && this.workingSettlers() == 0 ){
+		if( this.availableSettlers() == 0 && this.workingSettlers() == 0 && this.busySettlers() == 0){
 			this.gameOver();
 		}
 	}
@@ -386,12 +386,109 @@ Game.prototype._updateObservableProp = function(obs, newData, clearUnusedKeys, a
 	obs(current);
 }
 
+/**
+ * Fade out a target element and fade in a second target element, optionally. Returns a Promise
+ * @param {string|jQuery} firstElemOrSelector - A string containing a valid jQuery selector or a jQuery object
+ * @param {string|jQuery} secondElemOrSelector - A string containing a valid jQuery selector or a jQuery object
+ * @param {integer|undefined} fadeOutSpeed - the speed (in ms) to fade out the first object (default: 300)
+ * @param {integer|undefined} fadeInSpeed - the speed (in ms) to fade in the second object (default: 300)
+ * @param {function|undefined} midTransitionCallback - Optional callback to perform between the transitions
+ * @return Promise
+ */
+Game.prototype._hideOneElemAndShowAnother = function(firstElemOrSelector, secondElemOrSelector, fadeOutSpeed, fadeInSpeed, midTransitionCallback){
+
+	var promise = new Promise(function(resolve, reject){
+
+		firstElemOrSelector = ( firstElemOrSelector instanceof jQuery ) ? firstElemOrSelector : $(firstElemOrSelector) ;
+		secondElemOrSelector = ( secondElemOrSelector instanceof jQuery ) ? secondElemOrSelector : $(secondElemOrSelector) ;
+		fadeOutSpeed = (fadeOutSpeed != undefined) ? fadeOutSpeed : 300 ;
+		fadeInSpeed = (fadeInSpeed != undefined) ? fadeInSpeed : 300 ;
+
+		firstElemOrSelector.fadeOut(fadeOutSpeed, function(){
+
+			if(midTransitionCallback && typeof midTransitionCallback === 'function'){
+				midTransitionCallback();
+			}
+
+			secondElemOrSelector.fadeIn(fadeInSpeed, function(){
+				resolve();
+			});
+
+		});
+
+	});
+
+	return promise;
+
+}
+
+/**
+ * Reveal a div from left to right. Returns a Promise
+ * @param {string|jQuery} targetElemOrSelector - A string containing a valid jQuery selector or a jQuery object
+ * @param {integer|undefined} revealSpeed - the speed (in ms) to reveal the target div (default: 2000)
+ * @param {integer|undefined} waitInSecondsBeforeReturning - the speed (in ms) to wait before returning (default: 0)
+ * @return Promise
+ */
+Game.prototype.revealText = function(targetElemOrSelector, revealSpeed, waitInSecondsBeforeReturning) {
+
+	var promise = new Promise(function(resolve, reject){
+
+		targetElemOrSelector = ( targetElemOrSelector instanceof jQuery ) ? targetElemOrSelector : $(targetElemOrSelector) ;
+		revealSpeed = (revealSpeed != undefined) ? revealSpeed : 2000 ;
+		waitInSecondsBeforeReturning = waitInSecondsBeforeReturning || 0;
+
+		var origPosition = targetElemOrSelector.css("position");
+		targetElemOrSelector.css("position", "relative");
+
+		var overlayDiv = $('<div></div>').css({
+			position : 'absolute',
+			top : '0px',
+			left : '-10%',
+			width : '110%',
+			height : '100%',
+		}).addClass('white-gradient-with-transparency').appendTo(targetElemOrSelector);
+		targetElemOrSelector.show();
+		overlayDiv.hide("slide", { direction: "right", easing: "linear" }, revealSpeed, function(){
+			
+			overlayDiv.remove();
+			targetElemOrSelector.css("position", origPosition);
+
+			setTimeout(function(){
+				resolve();
+			}, waitInSecondsBeforeReturning);
+
+		});
+
+	});
+
+	return promise;
+
+}
+
 //End utility functions
 
 Game.prototype.gameOver = function(){
 	console.log('No settlers left.');
 	this.pauseMainTimer();
 	this.isGameOver = 1;
+}
+
+Game.prototype.newGame = function() {
+	var self = this;
+	this._hideOneElemAndShowAnother(
+		"#newgame-content",
+		"#intro-content",
+		300,
+		300
+	).then(function(){
+		return self.revealText("#intro-s1-p1", 2000, 1000);
+	}).then(function(){
+		return self.revealText("#intro-s1-p2", 2000, 1000);
+	}).then(function(){
+		return self.revealText("#intro-s1-p3", 2000, 1000);
+	}).then(function(){
+		$("#intro-s1-buttons").fadeIn(500);
+	});
 }
 
 Game.prototype._setupDefaultTimeEvents = function(){
@@ -413,29 +510,29 @@ Game.prototype._getDefaultResources = function(){
 
 //This is just temporary stuff to stop JS errors
 Game.prototype.hideModal = function() {
-	console.log('foo');
+	console.log('NOT IMPLEMENTED YET');
 }
 Game.prototype.showModalClose = function() {
-	console.log('bar');
+	console.log('NOT IMPLEMENTED YET');
 }
 Game.prototype.modalWindowTitle = function() {
-	console.log('bar');
+	console.log('NOT IMPLEMENTED YET');
 }
 Game.prototype.modalWindowText = function() {
-	console.log('bar');
+	console.log('NOT IMPLEMENTED YET');
 }
 Game.prototype.showModalWindowFooter = function() {
-	console.log('bar');
+	console.log('NOT IMPLEMENTED YET');
 }
 Game.prototype.loadGame = function() {
-	console.log('bar');
+	console.log('NOT IMPLEMENTED YET');
 }
 Game.prototype.saveGame = function() {
-	console.log('bar');
+	console.log('NOT IMPLEMENTED YET');
 }
 Game.prototype.logMessages = function() {
-	console.log('bar');
+	console.log('NOT IMPLEMENTED YET');
 }
 Game.prototype.showFaq = function() {
-	console.log('bar');
+	console.log('NOT IMPLEMENTED YET');
 }
--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
 
 	//Hide the loader gif and show the load/save controls now that everything's...well, loaded
 	$("#loading").addClass("hidden");
-	$(".save-load-controls").removeClass("hidden");
+	//$(".save-load-controls").removeClass("hidden");
 	
 	//TEMPORARY:
-	$(".player-stat-row").removeClass("hidden");
+	//$(".player-stat-row").removeClass("hidden");
 
 	//Sneakily switch our hidden elements to a jQuery hide/show instead of CSS class
 	$(".hidden").hide().removeClass("hidden");
@@ -25,6 +25,12 @@ $(document).ready(function(){
 			game.minusAction();
 		}
 	});
+
+	$("button.btn").click(function(){
+		this.blur();
+	});
+
+	$(".onecol-row").fadeIn(400);
 
 });
 
